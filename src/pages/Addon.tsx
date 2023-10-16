4fe505450383e7cb7813fb01347a0d343620e404
Spyei <caio.salamao1234@gmail.com>
2023-10-16 18:46:50 -0300

    feat: added styles to addon page

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -1,8 +1,31 @@
 import { FC } from "react";
+import { Params, useParams } from "react-router-dom";
+import addons from "../../addons.json";
 
 const AddonPage: FC = () => {
-    return (
-        <div>oi</div>
+    const params = useParams<Params>();
+    const addonName = params.addon;
+
+    const addon = addons.find(addon => addon.link.replace("/", "") === addonName);
+
+    return addon ? (
+        <section className="w-[100vw] max-w-[1500vw] flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center text-center p-3">
+                <div className="flex justify-center items-center flex-col w-full">
+                    <h1 className="text-white text-[100px] font-bold">{addon.title.toUpperCase()}</h1>
+                    <div className="text-white text-[20px] w-[50%]">{addon.description}</div>
+                </div>
+                <div className="flex flex-col w-full items-center justify-center">
+                    <h1 className="my-10 text-white font-bold text-[50px]">Showcase video</h1>
+                    <a href={addon.video} target="_blank">
+                        <img src={addon.image} />
+                    </a>
+                    <a href={addon.download} target="_blank" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px]">Download Addon</a>
+                </div>
+            </div>
+        </section>
+    ) : (
+        <div>Loading...</div>
     )
 }
 
