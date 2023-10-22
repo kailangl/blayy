cea9c3b52c295656cd0078e9e0a03fcda6e840bd
Spyei <caio.salamao1234@gmail.com>
2023-10-22 12:44:58 -0300

    feat: made images responsive

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -6,7 +6,7 @@ const AddonPage: FC = () => {
     const params = useParams<Params>();
     const addonName = params.addon;
 
-    const addon = addons.addons.find(addon => addon.link.replace("/", "") === addonName);
+    const addon = addons.addons.find(addon => addon.link.replace("/addon/", "") === addonName);
 
     return addon ? (
         <section className="w-[100vw] max-w-[1500vw] flex items-center justify-center overflow-x-hidden">
@@ -18,14 +18,14 @@ const AddonPage: FC = () => {
                 <div className="flex flex-col w-full items-center justify-center">
                     <h1 className="my-10 text-white font-bold tablet:text-[30px] text-[50px]">Showcase video</h1>
                     <a href={addon.video} target="_blank">
-                        <img src={addon.image} />
+                        <img className="max-w-[500px] tablet:w-[95vw]" src={addon.image} />
                     </a>
                     <a href={addon.download} target="_blank" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]">Download Addon</a>
                 </div>
             </div>
         </section>
     ) : (
-        <div>Loading...</div>
+        <div>this addon dont exists</div>
     )
 }
 
