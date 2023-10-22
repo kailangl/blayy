2e1c17b4627d5dde824869eb1b50749031c4eb4f
Spyei <caio.salamao1234@gmail.com>
2023-10-22 12:44:34 -0300

    feat: implementing this page to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import InitialPage from "./components/InitialPage";
 import AddonPage from "./pages/Addon";
+import AddonPackPage from "./pages/AddonPack";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<InitialPage/>} />
-            <Route path="/:addon" element={<AddonPage/>} />
+            <Route path="/addon/:addon" element={<AddonPage/>} />
+            <Route path="/addonpack/:addon" element={<AddonPackPage/>} />
         </Routes>
     )
 }
