3639893d7bb3690b27a8ee49f9b5be8a25fbbf3b
kailangl <157406748+kailangl@users.noreply.github.com>
2024-01-24 15:08:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import InitialPage from "./components/InitialPage";
+import AboutPage from "./pages/About";
 import AddonPage from "./pages/Addon";
 import AddonPackPage from "./pages/AddonPack";
-import AboutPage from "./pages/About";
 function App() {
     return (
         <main className="overflow-x-hidden">
