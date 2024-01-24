ec29f71491da341d1c236516f4089ea95173871e
kailangl <157406748+kailangl@users.noreply.github.com>
2024-01-24 14:37:08 -0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,2 +1,10 @@
 import { FC } from "react";
 import { Params, useParams } from "react-router-dom";
+
+const AboutPage: FC = () => {
+    const params = useParams<Params>();
+
+  
+}
+
+export default AboutPage;
