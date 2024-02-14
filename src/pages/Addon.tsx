99e0788a27cf056461ebaa0994996dcc454def6d
kailangl <157406748+kailangl@users.noreply.github.com>
2024-02-14 16:34:29 -0300

    Update Addon.tsx

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -14,15 +14,17 @@ const AddonPage: FC = () => {
             <section className="w-[100vw] max-w-[1500vw] flex items-center justify-center overflow-x-hidden">
                 <div className="flex flex-col items-center justify-center text-center p-3">
                     <h1 className="text-white text-[20px] w-[50%] tablet:w-[95vw]">-+ {addon.tag} +-</h1>
-                <a href="/" target="_blank" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]">HOME</a>
+                <a href="/" target="" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]">HOME</a>
                     <div className="flex justify-center items-center flex-col w-full">
                         <h1 className="text-white tablet:text-[10vw] text-[100px] font-bold">{addon.title.toUpperCase()}</h1>
                         <div id="container-57a06625508245eafda31519bf3468fb"></div>
                         <div className="text-white text-[20px] w-[50%] tablet:w-[95vw]">{addon.description}</div>
                     </div>
                     <div className="flex flex-col w-full items-center justify-center">
-                        <h1 className="my-10 text-white font-bold tablet:text-[30px] text-[50px]">Showcase video</h1>   
-                        
+                        <h1 className="my-10 text-white font-bold tablet:text-[30px] text-[50px]">Showcase video</h1>
+                        <iframe width="560" height="315" src={addon.video} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>                  
+                          
+                       
                         <a href={addon.download} target="_blank" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]">Download Addon</a>
                         <h1 className="text-white text-[20px] w-[50%] tablet:w-[95vw]">-+ BlayyDeveloper +-</h1>
                     </div>
