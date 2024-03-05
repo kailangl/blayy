22dbf18c2c5cf10abc52b0cdb2114addc0f2a788
kailangl <blayrandomxxx@gmail.com>
2024-03-05 20:25:12 -0300

    sitemobile

--- src/pages/Wiki.tsx
+++ src/pages/Wiki.tsx
@@ -20,8 +20,8 @@ const WikiPage: FC = () => {
             <div className="text-white text-[20px] w-[50%] tablet:w-[95vw]">{wiki1.recipe}</div>
             <div className="flex flex-col w-full items-center justify-center">
                         <h1 className="my-10 text-white font-bold tablet:text-[30px] text-[50px]">Images</h1>
-                     <img src={wiki1.image} alt="" className="w-128 h-72"/>
-                     <img src={wiki1.image2} alt="" className="w-128 h-72" />
+                     <img src={wiki1.image} alt="" className="max-w-[500px] tablet:w-[95vw]"/>        
+                     <img src={wiki1.image2} className="max-w-[500px] tablet:w-[95vw]" />
                     </div>
                     <h1 className="my-5 text-white font-bold tablet:text-[30px] text-[25px]">- from {wiki1.addon} -</h1>
                     <a href={wiki1.addonlink} target="_blank" className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transtion-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]">Addon Link</a>
