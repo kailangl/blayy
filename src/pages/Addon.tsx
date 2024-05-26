bed7889baf4fe6fb5c3906e8ccc6a9756818dc73
kailangl <157406748+kailangl@users.noreply.github.com>
2024-05-26 16:10:55 -0300

    Update Addon.tsx

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -55,7 +55,7 @@ const AddonPage: FC = () => {
         target="_blank"
         className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
       >
-        Download Addon (mcpedl safe)
+        Download Addon - Lootlabs (mcpedl safe)
       </a>
     )}                      
      {addon.downloadl && (
@@ -64,7 +64,7 @@ const AddonPage: FC = () => {
         target="_blank"
         className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
       >
-        Download Addon (Linkvertise)
+        Download Addon - Linkvertise
       </a>
     )}                       
     
