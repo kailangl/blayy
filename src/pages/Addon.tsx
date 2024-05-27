891aff3ec01919da481a584532190007a6061714
kailangl <157406748+kailangl@users.noreply.github.com>
2024-05-27 13:50:18 -0300

    Update Addon.tsx

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -51,7 +51,7 @@ const AddonPage: FC = () => {
     )}
       {addon.downloadll && (
       <a
-        href={addon.download}
+        href={addon.downloadll}
         target="_blank"
         className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
       >
