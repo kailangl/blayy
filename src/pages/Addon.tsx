cee1f903432374402609aeae51dddd0a221097ff
kailangl <157406748+kailangl@users.noreply.github.com>
2024-10-05 18:56:37 -0300

    Update Addon.tsx

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -76,7 +76,16 @@ const AddonPage: FC = () => {
       >
         Download Addon - Linkvertise
       </a>
-    )}                       
+    )} 
+                             {addon.vip && (
+      <a
+        href={addon.downloadl}
+        target="_blank"
+        className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
+      >
+        Direct Download (VIP)
+      </a>
+    )}           
     
   </div>
                      
