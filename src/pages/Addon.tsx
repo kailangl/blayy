dbbed2010113418acda11d0121725f85ea50b970
kailangl <157406748+kailangl@users.noreply.github.com>
2024-10-05 18:57:47 -0300

    Update Addon.tsx

--- src/pages/Addon.tsx
+++ src/pages/Addon.tsx
@@ -63,7 +63,7 @@ const AddonPage: FC = () => {
       <a
         href={addon.download}
         target="_blank"
-        className="hover:bg-blueborder shadow-blueshadow shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
+        className="hover:bg-blueborder shadow-bluetext shadow-xl hover:text-black transition-colors duration-300 border-4 border-blueborder text-bluetext cursor-pointer bg-bluebg text-center p-2 px-4 font-bold flex flex-grow justify-center mt-10 w-[400px] tablet:w-[95vw]"
       >
         Download Addon
       </a>
